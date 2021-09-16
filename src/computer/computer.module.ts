2edb2f02db9e0636e32191358958b6ca702d4d90
carycheng <carycheng77@gmail.com>
2021-09-15 20:32:47 -0700

    finished di

--- src/computer/computer.module.ts
+++ src/computer/computer.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ComputerController } from './computer.controller';
+import { CpuModule } from '../cpu/cpu.module';
+import { DiskModule } from '../disk/disk.module';
 
+// Every Controller or Service or Repository has a module. A module determines what is imported
+// and exported and is used for dependency injection.
+
+// Modules tag creates a DI Container.
+
+// Imports is similar to saying go into imported module and get everything that is exported so
+// our service or controller or repository can use it.
 @Module({
-  controllers: [ComputerController]
+  imports: [CpuModule, DiskModule],
+  controllers: [ComputerController],
 })
 export class ComputerModule {}
